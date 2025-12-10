f2a34e2796bc3b04eb23c5103dc4426652b411b8
Artur <111754782+btvvwtd@users.noreply.github.com>
2025-12-10 17:32:48 +0100

    Delete src directory

--- src/model.py
+++
@@ -1,7 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-
-def train_model(X_train, y_train):
-    """Trenuje prosty model klasyfikacji."""
-    clf = LogisticRegression(max_iter=200)
-    clf.fit(X_train, y_train)
-    return clf
